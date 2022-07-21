d4f9e503527a7a79405b49cf558955ab15d9910a
soniarez <101676781+soniarez@users.noreply.github.com>
2022-07-21 12:32:05 -0400

    router base

---
+++ src/views/Chef.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chef = () => {
+  return (
+    <div>
+      <h1>Queen´s Road Cafe CHEF</h1>
+      <p>Servers input</p>
+    </div>
+  );
+};
+
+export default Chef;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,8 @@
-import logo from "../assets/cafeLogo.png";
-import chef from "../assets/chef.png";
-import waiter from "../assets/waiter.png";
+import React from 'react';
+import logo from '../assets/cafeLogo.png';
+import chef from '../assets/chef.png';
+import waiter from '../assets/waiter.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -8,18 +10,16 @@ const Home = () => {
       <img src={logo} alt="cafe logo" />
       <input type="text" placeholder="Server´s name" />
       <div className="btn-container">
-      <button>
-        <img className="main-icons"
-          src={chef}
-          alt="chef icon"
-        />
-      </button>
-      <button>
-        <img className="main-icons"
-          src={waiter}
-          alt="waiter icon"
-        />
-      </button>
+        <button>
+          <Link to="/Chef">
+            <img className="main-icons" src={chef} alt="chef icon" />
+          </Link>
+        </button>
+        <button>
+          <Link to="/Waiter">
+            <img className="main-icons" src={waiter} alt="waiter icon" />
+          </Link>
+        </button>
       </div>
     </div>
   );
