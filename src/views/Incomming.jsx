2f6ba2644f2cde7e94274d64ab685191c3373599
soniarez <101676781+soniarez@users.noreply.github.com>
2022-07-21 20:11:55 -0400

    basic views routing

---
+++ src/views/Incomming.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Incomming = () => {
+    return (
+        <div>
+            <h1>Queen´s Road Cafe INCOMMING</h1>
+            <p>Input Chef Name</p>
+        </div>
+    );
+};
+
+export default Incomming;
