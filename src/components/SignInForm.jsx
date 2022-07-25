a10046d49d41b3236ab9130819a3b08c26bf2d72
soniarez <101676781+soniarez@users.noreply.github.com>
2022-07-25 15:54:51 -0400

    login and sigup component

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -5,57 +5,62 @@ const SigninForm = () => {
   return (
     <div className="main">
       <input type="checkbox" id="chk" aria-hidden="true"></input>
-    
+
       <div className="signup">
         <form>
           <label for="chk" aria-hidden="true" className="form-label">
             Sign up
           </label>
-          <input
-            className="form-input"
-            type="text"
-            name="txt"
-            placeholder="User name"
-            required=""
-          ></input>
-          <input
-            className="form-input"
-            type="email"
-            name="email"
-            placeholder="Email"
-            required=""
-          ></input>
-          <input
-            className="form-input"
-            type="password"
-            name="pswd"
-            placeholder="Password"
-            required=""
-          ></input>
-          <button className="btn-signup">Sign up</button>
+
+          <div className="form-info">
+            <input
+              className="form-input"
+              type="text"
+              name="txt"
+              placeholder="User name"
+              required=""
+            ></input>
+            <input
+              className="form-input"
+              type="email"
+              name="email"
+              placeholder="Email"
+              required=""
+            ></input>
+            <input
+              className="form-input"
+              type="password"
+              name="pswd"
+              placeholder="Password"
+              required=""
+            ></input>
+            <button className="btn-signup">Sign up</button>
+          </div>
         </form>
       </div>
-      
+
       <div className="login">
         <form>
           <label for="chk" aria-hidden="true" className="form-label">
             Login
           </label>
-          <input
-            className="form-input"
-            type="email"
-            name="email"
-            placeholder="Email"
-            required=""
-          ></input>
-          <input
-            className="form-input"
-            type="password"
-            name="pswd"
-            placeholder="Password"
-            required=""
-          ></input>
-          <button className="btn-login">Login</button>
+          <div className="form-info">
+            <input
+              className="form-input"
+              type="email"
+              name="email"
+              placeholder="Email"
+              required=""
+            ></input>
+            <input
+              className="form-input"
+              type="password"
+              name="pswd"
+              placeholder="Password"
+              required=""
+            ></input>
+            <button className="btn-login">Login</button>
+          </div>
         </form>
       </div>
     </div>
@@ -63,4 +68,3 @@ const SigninForm = () => {
 };
 
 export default SigninForm;
-
