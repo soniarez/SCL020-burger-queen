84fe1051e55eff6d19e37d8ac39176458bf0e641
Francisca Caro <francisca@caro.cl>
2022-07-25 16:20:41 -0400

    breakfast menu

---
+++ src/components/CardMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CardMenu = () => {
+    return (
+        <div className="card-menu">
+            <img />
+            <p className="card-menu-description"></p>
+            <p className="card-menu-price"></p>
+        </div>
+    )
+}
+
+export default CardMenu;
